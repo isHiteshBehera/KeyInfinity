f62d91f893d1ab67e294366e1813e93051a2db05
Hitesh Behera <bhiteshme74@gmail.com>
2023-12-07 23:38:52 +0530

    feature: Random Reveal Effect

--- app/src/main/java/com/example/keyinfinity/MainActivity.kt
+++ app/src/main/java/com/example/keyinfinity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,7 +14,10 @@ import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     private lateinit var passwordTextView: TextView
+    private val handler = Handler()
+    private lateinit var runnable: Runnable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,25 +25,63 @@ class MainActivity : AppCompatActivity() {
         passwordTextView = findViewById(R.id.passwordTextView)
 
         val generateButton: Button = findViewById(R.id.generateButton)
-        generateButton.setOnClickListener { generatePassword() }
+        generateButton.setOnClickListener {
+            val targetPassword = generateRandomPassword(20)
+            startRandomLetterAnimation(targetPassword)
+        }
 
         val copyButton: Button = findViewById(R.id.copyPassword)
         copyButton.setOnClickListener { copyToClipboard() }
     }
 
-    private fun generatePassword() {
-        val password = generateRandomPassword(20)
-        passwordTextView.text = password
+    private fun startRandomLetterAnimation(targetPassword: String) {
+        var currentIndex = 0
+
+        runnable = object : Runnable {
+            override fun run() {
+                val currentText = passwordTextView.text.toString()
+                val newText = StringBuilder()
+
+                if (currentIndex < targetPassword.length) {
+                    newText.append(targetPassword.substring(0, currentIndex))
+                } else {
+                    newText.append(targetPassword)
+                }
+
+
+
+                val random = Random()
+                for(i in currentIndex until targetPassword.length) {
+                    newText.append(charset[random.nextInt(charset.size)])
+                }
+
+                passwordTextView.text = newText.toString()
+
+                if(currentText == targetPassword) {
+                    stopRandomLetterAnimation()
+                } else {
+                    currentIndex++
+                    handler.postDelayed(this, 50)
+                }
+            }
+        }
+
+        handler.post(runnable)
+
+
     }
 
+    private fun stopRandomLetterAnimation() {
+        handler.removeCallbacks(runnable)
+    }
+
+
     private fun generateRandomPassword(length: Int): String {
-        val charset = ('a'..'z') + ('A'..'Z') + ('0'..'9')
         val random = Random()
         val password = StringBuilder()
 
         repeat(length) {
-            val index = random.nextInt(charset.size)
-            password.append(charset[index])
+            password.append(charset[random.nextInt(charset.size)])
         }
 
         return password.toString()
@@ -52,6 +94,6 @@ class MainActivity : AppCompatActivity() {
         val clipData = ClipData.newPlainText("Password", password)
         clipboardManager.setPrimaryClip(clipData)
 
-        Toast.makeText(this, "Cpoied", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Copied", Toast.LENGTH_SHORT).show()
     }
 }
